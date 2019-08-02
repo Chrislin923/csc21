007fa8136bd48e8fcfdb0a05073fc96d032a457f
Chris Lin <Chrislin923>
2019-08-02 13:55:41 -0700

    Ready for grading

--- model/Boggle.java
+++ model/Boggle.java
@@ -1,9 +1,11 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Boggle {
 
   // TODO: Complete a Boggle game that will be used with two views
   //  1. A console based game with standard IO  (Boggle Two)
   //  2. An event-driven program with a graphical user interface (Boggle Three)
-  
+	
 }
